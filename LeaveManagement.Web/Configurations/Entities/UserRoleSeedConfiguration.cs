ab4944d9c7a3d896b481f16d5e4d59fef45879d7
Kiwoong Andrew Kim <kw2446@gmail.com>
2022-02-14 18:06:36 -0500

    Added User Security To App. End of Authentication and Authorization  with Identity. Register, Add role, Default User, Role, UserRole migration EmailSender by using papercut SMTP

---
+++ LeaveManagement.Web/Configurations/Entities/UserRoleSeedConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LeaveManagement.Web.Configurations.Entities
+{
+    public class UserRoleSeedConfiguration : IEntityTypeConfiguration<IdentityUserRole<string>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+        {
+            builder.HasData(
+               new IdentityUserRole<string>
+               {
+                   RoleId = "cac43a7e-f7bb-4458-babf-1add431ccbbf",
+                   UserId = "ce096625-302c-4e93-ab09-9971a45301cc"
+               },
+               new IdentityUserRole<string>
+               {
+                   RoleId = "cac43a8e-f7cb-4148-baaf-1acb431ccbbf",
+                   UserId = "1d48f016-fb16-4255-b83a-477f112a9d42"
+               }
+            );
+        }
+
+     
+    }
+}
